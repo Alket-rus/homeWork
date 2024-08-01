010d52b4ebbe62f2ef7699b35078f792794eef31
Alex <Alexey-Idan-151@mail.ru>
2024-08-01 08:49:09 +0300

    Исправлены мелкие ошибки

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,24 @@ public class Main {
         FileOperations fileOperations = new FileOperationsImpl();
         FamilyManager personManager = new FamilyManager(personTree, fileOperations);
 
-        // Создайте людей и установите отношения
+
         personManager.createPeople();
         personManager.establishRelationships();
         personManager.establishMarriages();
 
-        // Добавьте новые семьи
+
         personManager.addNewFamilies();
 
-        // Распечатайте информацию о семьях
+
         personManager.printFamilyInfo();
 
-        // Печать людей, отсортированных по возрасту
+
         System.out.println("Люди, отсортированные по возрасту:");
         personTree.getPeopleSortedByAge().forEach(person ->
                 System.out.println(person.getFullName() + " (Возраст: " + person.getAge() + ")")
         );
 
-        // Печать людей, отсортированных по имени
+
         System.out.println("Люди, отсортированные по имени:");
         personTree.getPeopleSortedByName().forEach(person ->
                 System.out.println(person.getFullName())
